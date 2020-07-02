9f68bede5c26ee7ca39fa4c7dd4e0383cb5792c0
Dmitry Ozhigin <Lesnoytishka@gmail.com>
2020-07-02 18:34:16 +0300

    Lesson6 Hello world EE - TomCat

---
+++ Java---web/src/HelloServlet.java
@@ -0,0 +1,13 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello world!");
+    }
+}
